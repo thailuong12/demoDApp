320c0c4ba4323d81482b83dad226cbda3ec95491
Infection <ltkhiem@apcs.vn>
2018-07-22 21:31:11 +0700

    update readme

--- test/Realestate.js
+++ test/Realestate.js
@@ -1,10 +1,10 @@
-var Realestate = artifacts.require("./RealEstate.sol");
+var RealEstate = artifacts.require("./RealEstate.sol");
 
 
-contract("Realestate", (account)=>{
-    it('test find function', ()=>{
-        return Realestate.deployed().then((i)=>{
-            return i.findSellerByHouseId(1)
+contract("RealEstate", (account)=>{
+    it('Test buy function', ()=>{
+        return RealEstate.deployed().then((i)=>{
+            return i.buyHouse(1)
         }).then(function(res){
            assert(res, false)
             console.log(res)
